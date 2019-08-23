9e9ba8f7c4f2af12100a194dc2a8f7aefa4c0aac
Pablo Andres <pabloando99@gmail.com>
2019-08-23 10:15:10 +0200

    Morgan package included as a middleware

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) MIDDLEWARES
+
+app.use(morgan('dev'));
+
 app.use(express.json()); // middleware
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware! 🐱‍🐉');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+// 2) ROUTE HANDLERS
+
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
@@ -94,6 +113,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
+
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -104,6 +125,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {
